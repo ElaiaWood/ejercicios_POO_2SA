c12620ddd4be232dc0fbb6c99dbbd3ac066deba4
Elias U Xool-Feria <75928953+ElaiaWood@users.noreply.github.com>
2021-10-03 22:29:21 -0500

    Ejercicio 7 v.2
    Arreglo del ejercicio 7 - Falta añadir los metodos

--- Ejer07.java
+++ Ejer07.java
@@ -2,40 +2,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ejer07 {
-    public Ejer07(){
+    public Ejer07() {
 
     }
 
     public static void main(String[] args) {
         //Atributos
         Scanner s = new Scanner(System.in);
-        int nNum,w=0,datos;
+        int nNum, w = 0, datos;
         boolean condicional = true;
 
         ArrayList numeros = new ArrayList();
 
-        do{
+        do {
             System.out.println("Cuantos numeros desea que sea la lista?");
             nNum = s.nextInt();
-            if(nNum>=10 && nNum<+100){
-                while (w<nNum){
-                    System.out.println((w+1)+".Ingrese un numero");
-                    datos = s.nextInt();
-                    if(numeros.contains(datos)){
+
+            while (w < nNum) {
+                System.out.println((w + 1) + ".Ingrese un numero");
+                datos = s.nextInt();
+                if (datos >= 10 && datos <=100) {
+                    if (numeros.contains(datos)) {
                         System.out.println("Este numero ya esta guardado. Intente nuevamente");
-                    } else{
+                    } else {
                         numeros.add(datos);
                         System.out.println("Anotado!");
                         w++;
                     }
+                } else {
+                    System.out.println("ERROR");
+                    System.out.println("Ingrese un numero entre el rango de 10 a 100\n");
                 }
-                System.out.println("Listado de numeros: "+ numeros);
                 condicional = false;
-            } else {
-                System.out.println("ERROR");
-                System.out.println("Ingrese un numero entre el rango de 10 a 100\n");
             }
-
-        } while (condicional);
+            System.out.println("Listado de numeros: " + numeros);
+        }while (condicional);
     }
 }
